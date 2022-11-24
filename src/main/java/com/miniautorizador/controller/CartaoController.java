8b00567fd0601f93e06b74f5cfc01053baefc967
Luis Carlos <luisscarlostec@gmail.com>
2022-11-24 00:05:44 -0300

    controller do cartão

---
+++ src/main/java/com/miniautorizador/controller/CartaoController.java
@@ -0,0 +1,46 @@
+package com.miniautorizador.controller;
+
+import com.miniautorizador.contract.CartaoContract;
+import com.miniautorizador.exception.CartaoDuplicadoException;
+import com.miniautorizador.exception.CartaoInexistenteException;
+import com.miniautorizador.schema.CartaoResponse;
+import com.miniautorizador.schema.CriarCartao;
+import com.miniautorizador.service.CartaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+public class CartaoController implements CartaoContract {
+
+    @Autowired
+    private CartaoService cartaoService;
+
+    @Override
+    public ResponseEntity<CartaoResponse> criarCartao(CriarCartao cartao) {
+        CartaoResponse response;
+        try {
+            response = cartaoService.criarCartao(cartao);
+            return new ResponseEntity<>(response, CREATED);
+
+        } catch (CartaoDuplicadoException e) {
+            response = new CartaoResponse(e.getMessage());
+            return new ResponseEntity<>(response, UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    @Override
+    public ResponseEntity<BigDecimal> obterSaldoCartao(String numeroCartao) {
+        try {
+            BigDecimal response = cartaoService.obterSaldoCartao(numeroCartao);
+            return new ResponseEntity<>(response, OK);
+
+        } catch (CartaoInexistenteException e) {
+            return new ResponseEntity<>(null, NOT_FOUND);
+        }
+    }
+}
