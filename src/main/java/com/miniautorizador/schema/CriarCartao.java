0a4b8c588b6ea3a1f83c219dcf6063c256158fcd
Luis Carlos <luisscarlostec@gmail.com>
2022-11-24 00:24:04 -0300

    validação de tamanho para o número do cartão

--- src/main/java/com/miniautorizador/schema/CriarCartao.java
+++ src/main/java/com/miniautorizador/schema/CriarCartao.java
@@ -5,13 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 public class CriarCartao {
 //TODO aceitar no máximo 16 caracteres
     @NotNull
-//    @Length(max = 16)
+    @Size(min = 16, message = "O cartão deve ter 16 números")
+    @Size(max = 16, message = "O cartão deve ter 16 números")
     @Schema(description = "Número do cartão", example = "6549873025634501", required = true)
     private String numeroCartao;
 
