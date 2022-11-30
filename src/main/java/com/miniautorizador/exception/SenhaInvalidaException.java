695a0391131a978371d4dfc1c62c5452370bb16e
Luis Carlos <luisscarlostec@gmail.com>
2022-11-30 17:14:30 -0300

    anotação ResponseStatus adicionada nas classes de Exception

--- src/main/java/com/miniautorizador/exception/SenhaInvalidaException.java
+++ src/main/java/com/miniautorizador/exception/SenhaInvalidaException.java
@@ -1,5 +1,9 @@
 package com.miniautorizador.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class SenhaInvalidaException extends RuntimeException {
 
     public SenhaInvalidaException() {
