e252c3cefdb184a4874c67e813ffb5c21d19193d
Luis Carlos <luisscarlostec@gmail.com>
2022-11-30 17:12:57 -0300

    anotação ResponseStatus adicionada nas classes de Exception

--- src/main/java/com/miniautorizador/exception/CartaoDuplicadoException.java
+++ src/main/java/com/miniautorizador/exception/CartaoDuplicadoException.java
@@ -1,8 +1,22 @@
 package com.miniautorizador.exception;
 
+import com.miniautorizador.schema.CriarCartao;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+@Getter
 public class CartaoDuplicadoException extends RuntimeException {
 
-    public CartaoDuplicadoException (String numeroCartao, String senha) {
-        super(numeroCartao + senha);
+    private final String numeroCartao;
+
+    private final String senha;
+
+    public CartaoDuplicadoException (CriarCartao cartao) {
+        super();
+        this.numeroCartao = cartao.getNumeroCartao();
+        this.senha = cartao.getSenha();
     }
+
 }
