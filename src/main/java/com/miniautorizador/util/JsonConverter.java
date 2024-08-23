bc0fe73026be76250ffb8a59a44325805a910d98
Luis Carlos <luisscarlostec@gmail.com>
2024-08-23 12:27:59 -0300

    feature: classe para conversão de objetos json

---
+++ src/main/java/com/miniautorizador/util/JsonConverter.java
@@ -0,0 +1,19 @@
+package com.miniautorizador.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JsonConverter {
+
+    private final ObjectMapper mapper;
+
+    public JsonConverter() {
+        this.mapper = new ObjectMapper();
+    }
+
+    public <T> T jsonParaObjetoJava(String json, Class<T> clazz) throws JsonProcessingException {
+        return mapper.readValue(json, clazz);
+    }
+}
